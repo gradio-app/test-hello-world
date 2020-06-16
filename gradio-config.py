0a20e2f6e1882fb841f4e1221ffae84ace8482cf
Abubakar Abid <a12d@stanford.edu>
2020-06-16 12:52:53 -0500

    Update gradio-config.py

--- gradio-config.py
+++ gradio-config.py
@@ -1,4 +1,6 @@
 from hello import hello 
 import gradio
 
-INTERFACE = gradio.Interface(fn=hello, inputs='textbox', outputs='textbox')
+io = gradio.Interface(fn=hello, inputs='textbox', outputs='textbox')
+
+io.launch()

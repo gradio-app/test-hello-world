df6bcf1b4d97efb61617c6bca2c9bc94f86b9511
Abubakar Abid <a12d@stanford.edu>
2020-06-17 13:09:05 -0500

    updated to provide server name

--- gradio-config.py
+++ gradio-config.py
@@ -1,9 +1,17 @@
 from hello import hello 
 import gradio
+import sys
 import time
 
-io = gradio.Interface(fn=hello, inputs='textbox', outputs='textbox')
+
+io = gradio.Interface(fn=hello, inputs='textbox', outputs='textbox', server_name='0.0.0.0')
 
 io.launch()
 
-time.sleep(60)
+try:
+    print("Entering infinite loop...")
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    sys.exit(0)
+

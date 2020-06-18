2d2bdcd82faa2158b93d9d3f0183da9403c6a487
Abubakar Abid <a12d@stanford.edu>
2020-06-17 21:36:25 -0500

    remove while loop

--- gradio-config.py
+++ gradio-config.py
@@ -1,16 +1,6 @@
 from hello import hello 
 import gradio
-import sys
-import time
 
 io = gradio.Interface(fn=hello, inputs='textbox', outputs='textbox', verbose=True, server_name='0.0.0.0')
 
 io.launch()
-
-try:
-    print("Entering infinite loop...")
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    sys.exit(0)
-

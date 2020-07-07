00e4fb2fd66a1c15a87a18bc40431dda18df5dc7
Dawood Khan <dawoodkhan82@gmail.com>
2020-07-07 17:17:07 -0400

    Update run-gradio.py

--- run-gradio.py
+++ run-gradio.py
@@ -1,6 +1,7 @@
 from hello import hello 
 import gradio
 
-io = gradio.Interface(fn=hello, inputs='textbox', outputs='textbox', verbose=True)
+io = gradio.Interface(fn=hello, inputs='textbox', outputs='textbox', verbose=True, title="Hello World",
+    description="Hello World Description")
 
 io.launch()

cc0ef669905ba08cf9bb3f0ae684b62fe6fe266a
Dawood Khan <dawoodkhan82@gmail.com>
2020-10-26 16:17:03 -0400

    Update run-gradio.py

--- run-gradio.py
+++ run-gradio.py
@@ -2,6 +2,6 @@ from hello import hello
 import gradio
 
 io = gradio.Interface(fn=hello, inputs='text', outputs='text', verbose=True, title='Hello World',
-    description='Hello World Description', thumbnail='https://github.com/gradio-app/gradio_static/blob/master/static/img/logo.png?raw=true')
+    description='Hello World Description', thumbnail='https://github.com/gradio-app/gradio_static/blob/master/static/img/logo.png?raw=true', analytics_enabled=False)
 
 io.launch()

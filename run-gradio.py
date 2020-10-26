01f5260493b15b8277cb09ffbbfc2b57805999c5
Abubakar Abid <a12d@stanford.edu>
2020-10-26 17:13:14 -0500

    Update run-gradio.py

--- run-gradio.py
+++ run-gradio.py
@@ -1,6 +1,8 @@
 from hello import hello 
 import gradio
 
+print("TESTING")
+
 io = gradio.Interface(fn=hello, inputs='text', outputs='text', verbose=True, title='Hello World', 
     description='Hello World Description', thumbnail='https://github.com/gradio-app/gradio_static/blob/master/static/img/logo.png?raw=true', analytics_enabled=False)
 
